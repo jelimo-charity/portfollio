038e497d5a0ad1b0cc2609f8af7bfc91bda6a10a
jelimo-charity <charityjelimo893@gmail.com>
2024-03-12 14:59:25 +0300

    portfolio using react.js and tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import About from './components/About'
+import Contact from './components/Contact'
+import Navbar from './components/Navbar'
+import Projects from './components/Projects'
+import Skills from './components/Skills'
+
+function App() {
+  return (
+    <>
+    <Navbar />
+    <About />
+    <Projects />
+    <Skills />
+    <Contact />
+      
+    </>
+  )
+}
+
+export default App
