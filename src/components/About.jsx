4729d628691cb74ffb4e152420d392442a836d3f
jelimo-charity <charityjelimo893@gmail.com>
2024-07-03 16:48:39 +0300

    color change

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
     <section id='about' className="h-screen">
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center h-full">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center h-full justify-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-black">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-orange-600">
             Hi, I'm Jelimo.
           </h1>
           <p className="mb-8 leading-relaxed">
