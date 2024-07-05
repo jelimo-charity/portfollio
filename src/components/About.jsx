86ac12a2c6ab617df2380465d97779f529ac57b1
jelimo-charity <charityjelimo893@gmail.com>
2024-07-05 16:44:19 +0300

    modified about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,40 +1,44 @@
-import developerActivity from '../assets/images/me.png'
+import developerActivity from '../assets/images/me.png';
 
 const About = () => {
   return (
-    <section id='about' className="h-screen">
-      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center h-full">
-        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center h-full justify-center">
+    <section
+      id="about"
+      className="min-h-screen bg-cover bg-center bg-no-repeat flex items-center"
+      style={{ backgroundImage: `url(${developerActivity})` }}
+    >
+      <div className="container mx-auto px-5 py-10 md:py-20 flex flex-col md:flex-row items-center justify-center h-full">
+        <div className="lg:flex-grow md:w-1/2 flex flex-col md:items-start md:text-left items-center text-center justify-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-orange-600">
             Hi, I'm Jelimo.
           </h1>
-          <p className="mb-8 leading-relaxed">
-            I am a passionate software developer with a love for creating amazing web applications. With a keen eye for design and a drive for perfection, I strive to build solutions that are not only functional but also visually appealing. From front-end interfaces to back-end logic, I enjoy every step of the development process and continuously seek to learn and improve my skills.
+          <p className="mb-8 leading-relaxed text-white">
+            I am a passionate software developer with a love for creating amazing web applications.
+            <br />
+            With a keen eye for design and a drive for perfection, I strive to build solutions that are not only functional but also
+            <br />
+            visually appealing. From front-end interfaces to back-end logic, I enjoy every step of the development process and
+            <br />
+            continuously seek to learn and improve my skills.
           </p>
           <div className="flex justify-center">
             <a
               href="mailto:charityjelimo893@gmail.com"
-              className="inline-flex text-white bg-orange-600 border-0 py-2 px-6 focus:outline-none hover:bg-orange-400 rounded text-lg">
+              className="inline-flex text-white bg-orange-600 border-0 py-2 px-6 focus:outline-none hover:bg-orange-400 rounded text-lg"
+            >
               Get in touch
             </a>
             <a
               href="#projects"
-              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg"
+            >
               My projects
             </a>
           </div>
         </div>
-        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-          <img
-            className="object-cover object-center rounded w-full"
-            alt="developer activity"
-            src={developerActivity}
-            style={{ maxHeight: '75vh' }}
-          />
-        </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,32 @@
-
 const Navbar = () => {
   return (
-    <header className="bg-gray-800 md:sticky top-0 z-10">
-      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a className="title-font font-medium text-white mb-4 md:mb-0">
-          <a href="#about" className="ml-3 text-xl">
+    <header className="bg-slate-950 md:sticky top-0 z-10">
+      <div className="container mx-auto p-5 flex flex-col md:flex-row items-center">
+        <div className="w-full flex justify-between items-center mb-4 md:mb-0">
+          <a href="#about" className="title-font font-medium text-white text-4xl">
             Charity Jelimo
           </a>
-        </a>
-        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-          <a href="#projects" className="mr-5 text-orange-600 hover:text-white">
+        </div>
+        <nav className="w-full flex flex-row md:flex-row md:justify-center md:space-x-10 items-center text-base">
+          <a href="#projects" className="text-orange-600  text-2xl  hover:text-white mb-2 md:mb-0">
             Projects
           </a>
-          <a href="#skills" className="mr-5 text-orange-600 hover:text-white">
+          <a href="#skills" className="text-orange-600 ml-5  text-2xl hover:text-white mb-2 md:mb-0">
             Skills
           </a>
-          {/* <a href="#contact" className="mr-5 text-orange-600 hover:text-white">
+          {/* <a href="#contact" className="text-orange-600 hover:text-white mb-2 md:mb-0">
             Contact
           </a> */}
+          <a
+            href="https://drive.google.com/file/d/15sJCjIXCeFNyvYfuvjxMUEjSbwTwQ4Jh/view?usp=sharing"
+            className="text-orange-600 hover:text-white ml-5 mb-2 md:mb-0 text-2xl"
+          >
+            Resume
+          </a>
         </nav>
-        <a
-          href="https://drive.google.com/file/d/15sJCjIXCeFNyvYfuvjxMUEjSbwTwQ4Jh/view?usp=sharing"
-          className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none text-orange-600 hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
-          Resume
-        </a>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
